a8cc0a9456f867131faa7b6b465553aca8c11475
Daniela Lizondo <danielalizondochoque@gmail.com>
2023-11-14 14:01:57 -0400

    UsuarioCln

--- Sis457ConsultorioOdontologico/ClnConsultorioOdontologico/UsuarioCln.cs
+++ Sis457ConsultorioOdontologico/ClnConsultorioOdontologico/UsuarioCln.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using CadConsultorioOdontologico;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,61 @@ namespace ClnConsultorioOdontologico
 {
     public class UsuarioCln
     {
+        public static int insertar(Usuario usuario)
+        {
+            using (var context = new LabConsultorioOdontologicoEntities())
+            {
+                context.Usuario.Add(usuario);
+                context.SaveChanges();
+                return usuario.id;
+            }
+        }
 
+        public static int actualizar(Usuario usuario)
+        {
+            using (var context = new LabConsultorioOdontologicoEntities())
+            {
+                var existente = context.Usuario.Find(usuario.id);
+                existente.usuario1 = usuario.usuario1;
+                existente.clave = usuario.clave;
+                existente.usuarioRegistro = usuario.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabConsultorioOdontologicoEntities())
+            {
+                var existente = context.Usuario.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static Usuario get(int id)
+        {
+            using (var context = new LabConsultorioOdontologicoEntities())
+            {
+                return context.Usuario.Find(id);
+            }
+        }
+
+        public static List<Usuario> listar()
+        {
+            using (var context = new LabConsultorioOdontologicoEntities())
+            {
+                return context.Usuario.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paUsuarioListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabConsultorioOdontologicoEntities())
+            {
+                return context.paUsuarioListar(parametro).ToList();
+            }
+        }
     }
 }
