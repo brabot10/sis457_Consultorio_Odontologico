305f093f83e994af9a5cb4558ce7344bdf31a00f
Daniela Lizondo <danielalizondochoque@gmail.com>
2023-11-15 23:04:53 -0400

    frmUsuario codigo

--- Sis457ConsultorioOdontologico/CpConsultorioOdontologico/FrmUsuario.Designer.cs
+++ Sis457ConsultorioOdontologico/CpConsultorioOdontologico/FrmUsuario.Designer.cs
@@ -38,12 +38,12 @@
             this.txtParametro = new System.Windows.Forms.TextBox();
             this.gbxLista = new System.Windows.Forms.GroupBox();
             this.dgvLista = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.lblCedulaIdentidad = new System.Windows.Forms.Label();
-            this.lblNombre = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.lblPersonal = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.cbxPersonal = new System.Windows.Forms.ComboBox();
@@ -120,7 +120,7 @@
             this.dgvLista.EnableHeadersVisualStyles = false;
             this.dgvLista.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(203)))), ((int)(((byte)(209)))));
             this.dgvLista.Location = new System.Drawing.Point(32, 44);
-            this.dgvLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvLista.Margin = new System.Windows.Forms.Padding(4);
             this.dgvLista.MultiSelect = false;
             this.dgvLista.Name = "dgvLista";
             this.dgvLista.ReadOnly = true;
@@ -145,75 +145,76 @@
             this.dgvLista.Size = new System.Drawing.Size(518, 174);
             this.dgvLista.TabIndex = 0;
             // 
-            // button1
+            // btnBuscar
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(124)))), ((int)(((byte)(127)))));
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Image = global::CpConsultorioOdontologico.Properties.Resources.per_buscar;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(404, 34);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 64);
-            this.button1.TabIndex = 38;
-            this.button1.Text = "Buscar";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(124)))), ((int)(((byte)(127)))));
+            this.btnBuscar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnBuscar.FlatAppearance.BorderSize = 2;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Image = global::CpConsultorioOdontologico.Properties.Resources.per_buscar;
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.Location = new System.Drawing.Point(404, 34);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(166, 64);
+            this.btnBuscar.TabIndex = 38;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
-            // lblCedulaIdentidad
+            // lblUsuario
             // 
-            this.lblCedulaIdentidad.AutoSize = true;
-            this.lblCedulaIdentidad.ForeColor = System.Drawing.Color.White;
-            this.lblCedulaIdentidad.Location = new System.Drawing.Point(16, 396);
-            this.lblCedulaIdentidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblCedulaIdentidad.Name = "lblCedulaIdentidad";
-            this.lblCedulaIdentidad.Size = new System.Drawing.Size(65, 16);
-            this.lblCedulaIdentidad.TabIndex = 39;
-            this.lblCedulaIdentidad.Text = "Usuario:";
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.ForeColor = System.Drawing.Color.White;
+            this.lblUsuario.Location = new System.Drawing.Point(16, 396);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(65, 16);
+            this.lblUsuario.TabIndex = 39;
+            this.lblUsuario.Text = "Usuario:";
             // 
-            // lblNombre
+            // lblClave
             // 
-            this.lblNombre.AutoSize = true;
-            this.lblNombre.ForeColor = System.Drawing.Color.White;
-            this.lblNombre.Location = new System.Drawing.Point(18, 423);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(51, 16);
-            this.lblNombre.TabIndex = 40;
-            this.lblNombre.Text = "Clave:";
+            this.lblClave.AutoSize = true;
+            this.lblClave.ForeColor = System.Drawing.Color.White;
+            this.lblClave.Location = new System.Drawing.Point(18, 423);
+            this.lblClave.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(51, 16);
+            this.lblClave.TabIndex = 40;
+            this.lblClave.Text = "Clave:";
             // 
-            // label1
+            // lblPersonal
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(16, 454);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 16);
-            this.label1.TabIndex = 41;
-            this.label1.Text = "Asignar al Dr:";
+            this.lblPersonal.AutoSize = true;
+            this.lblPersonal.ForeColor = System.Drawing.Color.White;
+            this.lblPersonal.Location = new System.Drawing.Point(16, 454);
+            this.lblPersonal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPersonal.Name = "lblPersonal";
+            this.lblPersonal.Size = new System.Drawing.Size(101, 16);
+            this.lblPersonal.TabIndex = 41;
+            this.lblPersonal.Text = "Asignar al Dr:";
             // 
-            // textBox1
+            // txtClave
             // 
-            this.textBox1.Location = new System.Drawing.Point(159, 420);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
-            this.textBox1.MaxLength = 250;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(367, 22);
-            this.textBox1.TabIndex = 43;
+            this.txtClave.Location = new System.Drawing.Point(159, 420);
+            this.txtClave.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtClave.MaxLength = 250;
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(367, 22);
+            this.txtClave.TabIndex = 43;
             // 
-            // textBox2
+            // txtUsuario
             // 
-            this.textBox2.Location = new System.Drawing.Point(159, 388);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
-            this.textBox2.MaxLength = 250;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(367, 22);
-            this.textBox2.TabIndex = 44;
+            this.txtUsuario.Location = new System.Drawing.Point(159, 388);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtUsuario.MaxLength = 250;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(367, 22);
+            this.txtUsuario.TabIndex = 44;
             // 
             // btnGuardar
             // 
@@ -232,6 +233,7 @@
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnCancelar
             // 
@@ -250,6 +252,7 @@
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // cbxPersonal
             // 
@@ -283,19 +286,19 @@
             this.Controls.Add(this.cbxPersonal);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.lblNombre);
-            this.Controls.Add(this.lblCedulaIdentidad);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.lblPersonal);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.gbxLista);
             this.Controls.Add(this.txtParametro);
             this.Controls.Add(this.lblBusqueda);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmUsuario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmUsuario";
@@ -314,12 +317,12 @@
         private System.Windows.Forms.TextBox txtParametro;
         private System.Windows.Forms.GroupBox gbxLista;
         private System.Windows.Forms.DataGridView dgvLista;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label lblCedulaIdentidad;
-        private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.Label lblPersonal;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.ComboBox cbxPersonal;
--- Sis457ConsultorioOdontologico/CpConsultorioOdontologico/FrmUsuario.cs
+++ Sis457ConsultorioOdontologico/CpConsultorioOdontologico/FrmUsuario.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using CadConsultorioOdontologico;
+using ClnConsultorioOdontologico;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,14 +15,140 @@ namespace CpConsultorioOdontologico
 {
     public partial class FrmUsuario : Form
     {
+        bool esNuevo = false;
         public FrmUsuario()
         {
             InitializeComponent();
         }
 
+            private void listar()
+            {
+                var usuario = UsuarioCln.listarPa(txtParametro.Text.Trim());
+                dgvLista.DataSource = usuario;
+                dgvLista.Columns["id"].Visible = false;
+                dgvLista.Columns["estado"].Visible = false;
+                dgvLista.Columns["usuario"].HeaderText = "Usuario";
+                dgvLista.Columns["clave"].HeaderText = "Clave";
+                dgvLista.Columns["usuarioRegistro"].HeaderText = "Usuario";
+                dgvLista.Columns["fechaRegistro"].HeaderText = "Fecha del Registro";
+                if (usuario.Count > 0) dgvLista.Rows[0].Cells["usuario"].Selected = true;
+
+            }
         private void FrmUsuario_Load(object sender, EventArgs e)
         {
-
+            listar();
+            cargarpersonal();
         }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            //Establecer DialogResult como OK para indicar que se cerró correctamente
+            this.DialogResult = DialogResult.OK;
+
+            //Cerrar el fot¿rmulario
+            this.Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            listar();
+        }
+
+        public void cargarpersonal()
+        {
+            cbxPersonal.DataSource = PersonalCln.listar();
+            cbxPersonal.DisplayMember = "nombres";
+            cbxPersonal.ValueMember = "id";
+        }
+
+        private void txtPrametro_keyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) listar();
+        }
+        private bool validar()
+        {
+            bool esValido = true;
+            erpUsuario.SetError(txtUsuario, "");
+            erpClave.SetError(txtClave, "");
+            if (string.IsNullOrEmpty(txtUsuario.Text))
+            {
+                esValido = false;
+                erpUsuario.SetError(txtUsuario, "El campo Usuario es obligatorio");
+            }
+            if (string.IsNullOrEmpty(txtClave.Text))
+            {
+                esValido = false;
+                erpClave.SetError(txtClave, "El campo Clave es obligatorio");
+            }
+            return esValido;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (validar())
+            {
+                var usuario = new Usuario();
+                usuario.usuario1 = txtUsuario.Text.Trim();
+                usuario.clave = txtClave.Text.Trim();
+                usuario.usuarioRegistro = "SIS324";
+
+                var existeUsuario= UsuarioCln.Listar();
+                bool usuarioExiste = false;
+
+                foreach (var existeUsuarios in existeUsuario)
+                {
+                    if (existeUsuarios.usuario1 == usuario.usuario1 && (esNuevo || existeUsuarios.id != usuario.id))
+                    {
+                        usuarioExiste = true;
+                        break;
+                    }
+                }
+
+                if (usuarioExiste)
+                {
+                    MessageBox.Show("Ya existe un usuario con el mismo Nombre.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (esNuevo)
+                {
+                    usuario.fechaRegistro = DateTime.Now;
+                    usuario.estado = 1;
+                    usuario.idPersonal = Convert.ToInt32(cbxPersonal.SelectedValue);
+                    UsuarioCln.insertar(usuario);
+                }
+                else
+                {
+                    int index = dgvLista.CurrentCell.RowIndex;
+                    usuario.id = Convert.ToInt32(dgvLista.Rows[index].Cells["id"].Value);
+                    UsuarioCln.actualizar(usuario);
+                }
+                listar();
+                MessageBox.Show("Usuario guardado correctamente", "::: Consultorio Odontologico - Mensaje::: ",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void limpiar()
+        {
+            txtUsuario.Text = string.Empty;
+        }
+
+        int posX = 0;
+        int posY = 0;
+
+        private void pnlMovimiento_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                posX = e.X;
+                posY = e.Y;
+            }
+            else
+            {
+                Left = Left + (e.X - posX);
+                Top = Top + (e.Y - posY);
+            }
+        }
+            
     }
 }
