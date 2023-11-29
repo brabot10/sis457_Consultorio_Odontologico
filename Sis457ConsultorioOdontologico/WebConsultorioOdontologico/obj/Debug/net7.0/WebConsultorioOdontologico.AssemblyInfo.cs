024ae10e992db73a54c09ce8ac3bf54852bd259d
Daniela Lizondo <danielalizondochoque@gmail.com>
2023-11-29 19:19:44 -0400

    Index

--- Sis457ConsultorioOdontologico/WebConsultorioOdontologico/obj/Debug/net7.0/WebConsultorioOdontologico.AssemblyInfo.cs
+++ Sis457ConsultorioOdontologico/WebConsultorioOdontologico/obj/Debug/net7.0/WebConsultorioOdontologico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebConsultorioOdontologico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b80790e829e4c7e6038a642d461568220e754b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebConsultorioOdontologico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebConsultorioOdontologico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
